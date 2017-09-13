8efaf657a9fa086e1f40b54ddd11a50ba20e85a3
Eoras <pauldsb@outlook.fr>
2017-09-13 18:56:31 +0200

    Mise en page pour commencer....

--- index.php
+++ index.php
@@ -1,90 +1,86 @@
-<?php
-    function chargerClasse($classe) {
-        require("class/".$classe.".php");
-    }
-
-    spl_autoload_register('chargerClasse');
-
-    session_start();
-?>
-
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="en">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-
-
-    <meta name='description' content='A FAIRE'/>
-    <meta name='keywords' content='A FAIRE'/>
-    <meta name='author' content='WCS pierre paul steven'/>
-    <meta name='Copyright' content='Copyright : Wcs pierre paul steven' />
-    <meta name='geo.placename' content='Orleans, Centre, FRANCE' />
-    <meta name='Identifier-url' content='A FAIRE'/>
-    <meta name='geo.region' content='FR-FR' />
-
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
     <title>Wild Games</title>
-
-    <link rel="stylesheet" type="text/css" media="all" href="css.css" />
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
-
+        <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.css" rel="stylesheet">
+        <!-- Custom styles for this template -->
     <link href="css/style.css" rel="stylesheet">
-
 </head>
 
 <body>
 
-<nav class="navbar navbar-default">
-    <div class="container-fluid">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-            <!-- Burger Button -->
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
+<div class="container-fluid background3">
+
+    <nav class="navbar navbar-default nav-transparent">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+
+                <!-- Burger Button -->
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <a class="navbar-brand" href="#">
+                    <img alt="Brand" src="https://goo.gl/h79TT3">
+                </a>
+                <ul class="nav navbar-nav text-center">
+                    <li><a href="#">ACCUEIL</a></li>
+                    <li><a href="#">JEUX</a></li>
+                    <li class="enabled"><a href="#">SNACKING-BAR</a></li>
+                    <li><a href="#">INFORMATIONS PRATIQUES</a></li>
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav>
+
+<div class="container-fluid">
+
+    <div class="row">
+
+        <div class="col-md-3 test">
+            <a href="#" class="thumbnail">
+                <img src="https://goo.gl/yynDFZ" alt="test">
+            </a>
         </div>
 
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav text-center">
-                <li><a href="#">ACCUEIL</a></li>
-                <li><a href="#">JEUX</a></li>
-                <li><a href="#">SNACKING-BAR</a></li>
-                <li><a href="#">INFORMATIONS PRATIQUES</a></li>
-            </ul>
-        </div><!-- /.navbar-collapse -->
-    </div><!-- /.container-fluid -->
-</nav>
+        <div class="col-md-6 test">
+            <div class="panel panel-default">
+                <div class="panel-heading">Liste de mes items</div>
+                <div class="panel-body">
+                    Panel content <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+                </div>
+                <div class="panel-footer">Footer de mon panel</div>
+            </div>
+        </div>
 
-<!-- TRAVAIL PIERRE -->
-<section>
+        <div class="col-md-3 test">
+            <a href="#" class="thumbnail">
+                <img src="https://goo.gl/yynDFZ" alt="test">
+            </a>
+        </div>
 
-
-</section>
-
-
-
-<!-- TRAVAIL PAUL -->
-<section>
+    </div>
 
 
-</section>
-
-
-
-
-
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+</div>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
 <script src="js/bootstrap.js"></script>
 
 </body>
-
 </html>
