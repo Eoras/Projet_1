34dbad637601bf4a424bc92706b0c6f6ea66027c
Eoras <pauldsb@outlook.fr>
2017-09-14 15:31:24 +0200

    Ajout du milieu avec le menu et début des burger, le tableau fonctionne correctement

--- index.php
+++ index.php
@@ -40,7 +40,7 @@
                 <ul class="nav navbar-nav text-center">
                     <li><a href="#">ACCUEIL</a></li>
                     <li><a href="#">JEUX</a></li>
-                    <li class="enabled"><a href="#">SNACKING-BAR</a></li>
+                    <li class="enabled"><a href="#snackbar">SNACKING-BAR</a></li>
                     <li><a href="#">INFORMATIONS PRATIQUES</a></li>
                 </ul>
             </div><!-- /.navbar-collapse -->
@@ -51,18 +51,94 @@
 
     <div class="row">
 
-        <div class="col-md-3 test">
+        <div class="col-lg-3 hidden-md test" id="snackbar">
             <a href="#" class="thumbnail">
                 <img src="https://goo.gl/yynDFZ" alt="test">
             </a>
         </div>
 
-        <div class="col-md-6 test">
+        <div class="col-md-9 col-lg-6 test">
             <div class="panel panel-default">
-                <div class="panel-heading">Liste de mes items</div>
+                <div class="panel-heading" ><span class="glyphicon glyphicon-glass">&nbsp;</span>SNACK BAR MENU</div>
+
                 <div class="panel-body">
-                    Panel content <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+
+                    <div class="row">
+
+                        <div class="col-md-6 test">
+                            <div class="panel panel-default">
+                                <!-- Default panel contents -->
+                                <div class="panel-heading"></div>
+                                <table class="table">
+                                    <th>Nos Burgers</th><th>Prix</th>
+
+                                    <tr><td>
+                                            <div class="media">
+                                                <div class="media-left">
+                                                    <img class="media-object thumb" src="images/burgers/burger1.png" alt="burger">
+                                                </div>
+                                                <div class="media-body">
+                                                    <h5 class="media-heading">Burger 1</h5>
+                                                    <p>Pain rustique, Steaks hachés viande bovine française, Cheddar fondu, Bacon, Salade, Sauce tartare</p>
+                                                </div>
+                                            </div>
+                                        </td><td>16,10€</td></tr>
+
+
+                                    <tr><td>
+                                            <div class="media">
+                                                <div class="media-left">
+                                                    <img class="media-object thumb" src="images/burgers/burger2.png" alt="burger">
+                                                </div>
+                                                <div class="media-body">
+                                                    <h5 class="media-heading">Burger 2</h5>
+                                                    <p>Pain rustique, Steaks hachés viande bovine française , Raclette, Beignets d'oignon, Salade, Sauce Barbecue</p>
+                                                </div>
+                                            </div>
+                                        </td><td>6,10€</td></tr>
+
+                                    <tr><td>
+                                            <div class="media">
+                                                <div class="media-left">
+                                                    <img class="media-object thumb" src="images/burgers/burger3.png" alt="burger">
+                                                </div>
+                                                <div class="media-body">
+                                                    <h5 class="media-heading">Burger 3</h5>
+                                                    <p>Pain rustique, 2 Aiguillettes de Cabillaud croustillantes, Cheddar fondu, Oignons grillés, Salade, Sauce béarnaise</p>
+                                                </div>
+                                            </div>
+                                        </td><td>84,50€</td></tr>
+                                </table>
+                            </div>
+                        </div>
+
+                        <div class="col-md-6 test">
+                            <div class="panel panel-default">
+                                <!-- Default panel contents -->
+                                <div class="panel-heading">Nos Pizza</div>
+                                <table class="table">
+                                    <th></th><th>Prix</th>
+
+                                    <tr><td>Pizza 1
+                                            <br/><p>Pain rustique, Steaks hachés viande bovine française, Cheddar fondu, Bacon, Salade, Sauce tartare</p></td>
+                                        <td>16,10€ </td></tr>
+
+                                    <tr><td>Pizza 2
+                                            <br/><p>Pain rustique, Steaks hachés viande bovine française , Raclette, Beignets d'oignon, Salade, Sauce Barbecue</p></td>
+                                        <td>6,10€ </td></tr>
+
+                                    <tr><td>Pizza 3
+                                            <br/><p>Pain rustique, 2 Aiguillettes de Cabillaud croustillantes, Cheddar fondu, Oignons grillés, Salade, Sauce béarnaise</p></td>
+                                        <td>84,50€ </td></tr>
+
+                                </table>
+                            </div>
+                        </div>
+
+                    </div>
+
                 </div>
+
                 <div class="panel-footer">Footer de mon panel</div>
             </div>
         </div>
