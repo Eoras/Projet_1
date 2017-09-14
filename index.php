c40dc160cc3a2f60fdc1550c4e69cd4f76df95b8
Eoras <pauldsb@outlook.fr>
2017-09-14 15:38:01 +0200

    Ajout d'un contenair sur mon ensemble afin de ne pas avoir d'espaces sur le côté droit

--- index.php
+++ index.php
@@ -16,143 +16,115 @@
 
 <body>
 
-<div class="container-fluid background3">
+<!-- DEBUT DU CODE DE PAUL -->
+<div class="container-fluid backgroundsnack">
+    <div class="container-fluid">
 
-    <nav class="navbar navbar-default nav-transparent">
-        <div class="container-fluid">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
+        <div class="row">
 
-                <!-- Burger Button -->
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
+            <div class="col-lg-3 hidden-md test" id="snackbar">
+                <a href="#" class="thumbnail">
+                    <img src="https://goo.gl/yynDFZ" alt="test">
+                </a>
             </div>
 
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <a class="navbar-brand" href="#">
-                    <img alt="Brand" src="https://goo.gl/h79TT3">
-                </a>
-                <ul class="nav navbar-nav text-center">
-                    <li><a href="#">ACCUEIL</a></li>
-                    <li><a href="#">JEUX</a></li>
-                    <li class="enabled"><a href="#snackbar">SNACKING-BAR</a></li>
-                    <li><a href="#">INFORMATIONS PRATIQUES</a></li>
-                </ul>
-            </div><!-- /.navbar-collapse -->
-        </div><!-- /.container-fluid -->
-    </nav>
+            <div class="col-md-9 col-lg-6 test">
+                <div class="panel panel-default">
+                    <div class="panel-heading" ><span class="glyphicon glyphicon-glass">&nbsp;</span>SNACK BAR MENU</div>
 
-<div class="container-fluid">
+                    <div class="panel-body">
 
-    <div class="row">
+                        <div class="row">
 
-        <div class="col-lg-3 hidden-md test" id="snackbar">
-            <a href="#" class="thumbnail">
-                <img src="https://goo.gl/yynDFZ" alt="test">
-            </a>
-        </div>
+                            <div class="col-md-6 test">
+                                <div class="panel panel-default">
+                                    <!-- Default panel contents -->
+                                    <div class="panel-heading"></div>
+                                    <table class="table">
+                                        <th>Nos Burgers</th><th>Prix</th>
 
-        <div class="col-md-9 col-lg-6 test">
-            <div class="panel panel-default">
-                <div class="panel-heading" ><span class="glyphicon glyphicon-glass">&nbsp;</span>SNACK BAR MENU</div>
-
-                <div class="panel-body">
-
-                    <div class="row">
-
-                        <div class="col-md-6 test">
-                            <div class="panel panel-default">
-                                <!-- Default panel contents -->
-                                <div class="panel-heading"></div>
-                                <table class="table">
-                                    <th>Nos Burgers</th><th>Prix</th>
-
-                                    <tr><td>
-                                            <div class="media">
-                                                <div class="media-left">
-                                                    <img class="media-object thumb" src="images/burgers/burger1.png" alt="burger">
+                                        <tr><td>
+                                                <div class="media">
+                                                    <div class="media-left">
+                                                        <img class="media-object thumb" src="images/burgers/burger1.png" alt="burger">
+                                                    </div>
+                                                    <div class="media-body">
+                                                        <h5 class="media-heading">Burger 1</h5>
+                                                        <p>Pain rustique, Steaks hachés viande bovine française, Cheddar fondu, Bacon, Salade, Sauce tartare</p>
+                                                    </div>
                                                 </div>
-                                                <div class="media-body">
-                                                    <h5 class="media-heading">Burger 1</h5>
-                                                    <p>Pain rustique, Steaks hachés viande bovine française, Cheddar fondu, Bacon, Salade, Sauce tartare</p>
-                                                </div>
-                                            </div>
-                                        </td><td>16,10€</td></tr>
+                                            </td><td>16,10€</td></tr>
 
 
-                                    <tr><td>
-                                            <div class="media">
-                                                <div class="media-left">
-                                                    <img class="media-object thumb" src="images/burgers/burger2.png" alt="burger">
+                                        <tr><td>
+                                                <div class="media">
+                                                    <div class="media-left">
+                                                        <img class="media-object thumb" src="images/burgers/burger2.png" alt="burger">
+                                                    </div>
+                                                    <div class="media-body">
+                                                        <h5 class="media-heading">Burger 2</h5>
+                                                        <p>Pain rustique, Steaks hachés viande bovine française , Raclette, Beignets d'oignon, Salade, Sauce Barbecue</p>
+                                                    </div>
                                                 </div>
-                                                <div class="media-body">
-                                                    <h5 class="media-heading">Burger 2</h5>
-                                                    <p>Pain rustique, Steaks hachés viande bovine française , Raclette, Beignets d'oignon, Salade, Sauce Barbecue</p>
-                                                </div>
-                                            </div>
-                                        </td><td>6,10€</td></tr>
+                                            </td><td>6,10€</td></tr>
 
-                                    <tr><td>
-                                            <div class="media">
-                                                <div class="media-left">
-                                                    <img class="media-object thumb" src="images/burgers/burger3.png" alt="burger">
+                                        <tr><td>
+                                                <div class="media">
+                                                    <div class="media-left">
+                                                        <img class="media-object thumb" src="images/burgers/burger3.png" alt="burger">
+                                                    </div>
+                                                    <div class="media-body">
+                                                        <h5 class="media-heading">Burger 3</h5>
+                                                        <p>Pain rustique, 2 Aiguillettes de Cabillaud croustillantes, Cheddar fondu, Oignons grillés, Salade, Sauce béarnaise</p>
+                                                    </div>
                                                 </div>
-                                                <div class="media-body">
-                                                    <h5 class="media-heading">Burger 3</h5>
-                                                    <p>Pain rustique, 2 Aiguillettes de Cabillaud croustillantes, Cheddar fondu, Oignons grillés, Salade, Sauce béarnaise</p>
-                                                </div>
-                                            </div>
-                                        </td><td>84,50€</td></tr>
-                                </table>
+                                            </td><td>84,50€</td></tr>
+                                    </table>
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="col-md-6 test">
-                            <div class="panel panel-default">
-                                <!-- Default panel contents -->
-                                <div class="panel-heading">Nos Pizza</div>
-                                <table class="table">
-                                    <th></th><th>Prix</th>
+                            <div class="col-md-6 test">
+                                <div class="panel panel-default">
+                                    <!-- Default panel contents -->
+                                    <div class="panel-heading">Nos Pizza</div>
+                                    <table class="table">
+                                        <th></th><th>Prix</th>
 
-                                    <tr><td>Pizza 1
-                                            <br/><p>Pain rustique, Steaks hachés viande bovine française, Cheddar fondu, Bacon, Salade, Sauce tartare</p></td>
-                                        <td>16,10€ </td></tr>
+                                        <tr><td>Pizza 1
+                                                <br/><p>Pain rustique, Steaks hachés viande bovine française, Cheddar fondu, Bacon, Salade, Sauce tartare</p></td>
+                                            <td>16,10€ </td></tr>
 
-                                    <tr><td>Pizza 2
-                                            <br/><p>Pain rustique, Steaks hachés viande bovine française , Raclette, Beignets d'oignon, Salade, Sauce Barbecue</p></td>
-                                        <td>6,10€ </td></tr>
+                                        <tr><td>Pizza 2
+                                                <br/><p>Pain rustique, Steaks hachés viande bovine française , Raclette, Beignets d'oignon, Salade, Sauce Barbecue</p></td>
+                                            <td>6,10€ </td></tr>
 
-                                    <tr><td>Pizza 3
-                                            <br/><p>Pain rustique, 2 Aiguillettes de Cabillaud croustillantes, Cheddar fondu, Oignons grillés, Salade, Sauce béarnaise</p></td>
-                                        <td>84,50€ </td></tr>
+                                        <tr><td>Pizza 3
+                                                <br/><p>Pain rustique, 2 Aiguillettes de Cabillaud croustillantes, Cheddar fondu, Oignons grillés, Salade, Sauce béarnaise</p></td>
+                                            <td>84,50€ </td></tr>
 
-                                </table>
+                                    </table>
+                                </div>
                             </div>
+
                         </div>
 
                     </div>
 
+                    <div class="panel-footer">Footer de mon panel</div>
                 </div>
-
-                <div class="panel-footer">Footer de mon panel</div>
             </div>
-        </div>
 
-        <div class="col-md-3 test">
-            <a href="#" class="thumbnail">
-                <img src="https://goo.gl/yynDFZ" alt="test">
-            </a>
-        </div>
+            <div class="col-md-3 test">
+                <a href="#" class="thumbnail">
+                    <img src="https://goo.gl/yynDFZ" alt="test">
+                </a>
+            </div>
+
 
     </div>
-
-
 </div>
+<!-- FIN DU CODE DE PAUL -->
+
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
