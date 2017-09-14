e36b70c262cab581f868f5bfd87e9da8915fbb63
StevenB45 <stevendsb@gmail.com>
2017-09-14 15:40:58 +0200

    Début du navbar a merge

--- index.php
+++ index.php
@@ -1,14 +1,3 @@
-<?php
-function chargerClasse($classe)
-{
-    require("class/" . $classe . ".php");
-}
-
-spl_autoload_register('chargerClasse');
-
-session_start();
-?>
-
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -17,32 +6,34 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
+
     <meta name='description' content='A FAIRE'/>
     <meta name='keywords' content='A FAIRE'/>
     <meta name='author' content='WCS pierre paul steven'/>
-    <meta name='Copyright' content='Copyright : Wcs pierre paul steven'/>
-    <meta name='geo.placename' content='Orleans, Centre, FRANCE'/>
+    <meta name='Copyright' content='Copyright : Wcs pierre paul steven' />
+    <meta name='geo.placename' content='Orleans, Centre, FRANCE' />
     <meta name='Identifier-url' content='A FAIRE'/>
-    <meta name='geo.region' content='FR-FR'/>
+    <meta name='geo.region' content='FR-FR' />
 
     <title>Wild Games</title>
 
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
-
-    <link rel="stylesheet" href="css/style.css" >
-    <link rel="stylesheet" href="css/bootstrap.css" >
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
 
 </head>
 
-<body class="container-fluid">
+<body>
 
-    <nav class="navbar navbar-default">
+<!-- DEBUT PARTIE STEVEN PREMIERE PAGE --> 
+
+<div class="container-fluid fondsite">
+
+    <nav class="navbar navbar-default nav-transparent">
         <div class="container-fluid">
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
                 <!-- Burger Button -->
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-                        data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
                     <span class="sr-only">Toggle navigation</span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
@@ -52,67 +43,23 @@ session_start();
 
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav text-center">
-                    <li><a href="#">ACCUEIL</a></li>
-                    <li><a href="#">JEUX</a></li>
-                    <li><a href="#">SNACKING-BAR</a></li>
-                    <li><a href="#">INFORMATIONS PRATIQUES</a></li>
+                <ul class="nav navbar-nav text-center blink">
+                    <li><a href="#"># ACCUEIL</a></li>
+                    <li><a href="#"># JEUX</a></li>
+                    <li><a href="#"># SNACKING-BAR</a></li>
+                    <li><a href="#"># INFOS PRATIQUES</a></li>
                 </ul>
             </div><!-- /.navbar-collapse -->
         </div><!-- /.container-fluid -->
     </nav>
 
-    <!-- TRAVAIL PIERRE -->
-    <section id="pierre">
-
-        <div class="row">
-
-            <div class="col-xs-6 col-sm-4 col-md-3 col-lg-offset-1 col-lg-1">
-                <img src="http://via.placeholder.com/150x150" alt="descriptif_activite_1" />
-            </div>
-
-            <div class="col-xs-6 col-sm-4 col-md-3 col-lg-offset-1 col-lg-1">
-                <img src="http://via.placeholder.com/150x150" alt="descriptif_activite_2" />
-            </div>
-
-            <div class="col-xs-6 col-sm-4 col-md-3 col-lg-offset-1 col-lg-1">
-                <img src="http://via.placeholder.com/150x150" alt="descriptif_activite_" />
-            </div>
-
-            <div class="col-xs-6 col-sm-4 col-md-3 col-lg-offset-1 col-lg-1">
-                <img src="http://via.placeholder.com/150x150" alt="descriptif_activite_" />
-            </div>
-
-        </div>
-
-        <div class="row">
-
-            <div class="col-xs-6 col-sm-4 col-md-3 col-lg-offset-1 col-lg-1">
-                <p>"Lorem ipsum dolor sit amet, consectetnim ad minim veniam, roident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-            </div>
-
-            <div class="col-xs-6 col-sm-4 col-md-3 col-lg-offset-1 col-lg-1">
-                <p>"Lorem ipsum dolor sit amet, consectetnim ad minim veniam, roident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-            </div>
-
-            <div class="col-xs-6 col-sm-4 col-md-3 col-lg-offset-1 col-lg-1">
-                <p>"Lorem ipsum dolor sit amet, consectetnim ad minim veniam, roident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-            </div>
-
-            <div class="col-xs-6 col-sm-4 col-md-3 col-lg-offset-1 col-lg-1">
-                <p>"Lorem ipsum dolor sit amet, consectetnim ad minim veniam, roident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-            </div>
-
-        </div>
-
-    </section>
-
-
-    <!-- TRAVAIL PAUL -->
-    <section>
-
-
-    </section>
+    <div class="container accueil">
+        <section class="row">
+            <div class="col-xs-12"><img src="../images/Wild Games7.png" class="img_responsive center-block imglogo" alt="Wild Games" style="width:100%; height:auto; display:block; max-width:1000px"></div>
+        </section>
+    </div>
+</div>
+<!-- FIN PARTIE STEVEN PREMIERE PAGE --> 
 
 
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
